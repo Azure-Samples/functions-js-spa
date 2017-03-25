0b63be41f8ce1d4c25c9e2d6f7845411ec679310
Donna Malayeri <donnam@microsoft.com>
2017-03-24 17:51:58 -0700

    Minor tweaks

--- GetFunctionLogo/index.js
+++ GetFunctionLogo/index.js
@@ -3,17 +3,17 @@ module.exports = function (context, req) {
     const logoUrlFunctions = "https://donnamblobstorage.blob.core.windows.net/public/azure-functions-logo-smaller.png";
     const logoUrlServerless = "https://donnamblobstorage.blob.core.windows.net/public/azure-serverless.png";
 
-    const imageId = Math.floor(Math.random() * 4);
+    const imageId = Math.floor(Math.random() * 3);
     context.log(imageId);
 
     var imageResult = `<pre>
 <font face="consolas" color="#4674B5">             <font color="#FFFF00">%%%</font><font color="#FFC000">%%%</font>         <br>            <font color="#FFFF00">%%%</font><font color="#FFC000">%%%</font>          <br>       @   <font color="#FFFF00">%%%</font><font color="#FFC000">%%%</font>    @      <br>     @@   <font color="#FFFF00">%%%</font><font color="#FFC000">%%%</font>      @@    <br>  @@@    <font color="#FFFF00">%%%</font><font color="#FFC000">%%%%%%%%</font>    @@@ <br>@@      <font color="#FFFF00">%%%%%%%</font><font color="#FFC000">%%%</font>        @@<br>  @@         <font color="#FFFF00">%</font><font color="#FFC000">%%%</font>       @@  <br>    @@      <font color="#FFFF00">%</font><font color="#FFC000">%%</font>       @@    <br>      @@    <font color="#FFFF00">%</font><font color="#FFC000">%</font>      @@      <br>           <font color="#FFFF00">%</font><font color="#FFC000">%</font>               <br>           <font color="#FFC000">%               </font> 
 </font></pre>`;
 
-    if (imageId == 2) {
+    if (imageId == 1) {
         imageResult = `<img width="250px" src="${logoUrlFunctions}"></img>`;
     }
-    else if (imageId == 3) {
+    else if (imageId == 2) {
         imageResult = `<img width="400px" src="${logoUrlServerless}"></img>`;
     }
 
